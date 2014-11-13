4b5748bb8bec5f2df18b8911e9377a06996ab11b
Zaki Akhmad <za@python.or.id>
2014-11-14 10:21:59 +1100

    forms with few errors

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 #from django.template import render
 from django.shortcuts import render
 from rango.models import Category
+from rango.forms import CategoryForm
 
 def index(request):
     #context_dict = {'boldmessage': "I am the bold font from the context"}
@@ -13,3 +14,16 @@ def about(request):
 #    return HttpResponse("Rango says here is the about page. <a href='/rango'>index</a>")
     context_dict = {'boldmessage': "About..."}
     return render(request, 'rango/about.html', context_dict) 
+
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print form.errors
+    else:
+        form = CategoryForm()
+
+    return render(request, 'rango/add_category.html', {'form': form})
